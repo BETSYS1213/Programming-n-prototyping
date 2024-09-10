2280836f8865dbebef7c39197685f777cf5ad6ec
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-09-10 08:52:55 -0400

    Update and rename fname S to Unit1/CFU1.py

---
+++ Unit1/CFU1.py
@@ -0,0 +1,11 @@
+#9/10/2024
+#2
+#Betsy Sumba
+
+
+question = input("what your first name ")
+
+print( "Hi " + question + "!" )
+
+question = input("what your last name ")
+print( " Hello " + question +  "!")
