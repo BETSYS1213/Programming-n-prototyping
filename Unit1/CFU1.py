086c07bfbf21256ab94fe868cdb8f9ecfa333d5c
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-09-16 09:22:33 -0400

    Update CFU1.py

--- Unit1/CFU1.py
+++ Unit1/CFU1.py
@@ -7,3 +7,9 @@ question = ("what's your fisrt day")
 question2 = ("what's your last day")
 fullname = question + question
 print(fullname)
+
+question = input ("What your first name")
+question = input ("What your last name")
+question = input ("how old you are")
+question = input ("how long you been eating")
+print(" Hello Betsy " + "Sumba ")
