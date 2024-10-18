4f03b95ac09720684695a481e2872be441a80d47
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-10-18 09:36:30 -0400

    Update practice.py

--- unit3/practice.py
+++ unit3/practice.py
@@ -34,3 +34,39 @@ if delivery == "yes" or delivery == "yes" :
     
 else:
     print("NO?! so who is cooking?")
+
+
+
+
+#Betsy Sumba
+#10/18/2024
+# 1 and 2
+
+import random
+print("lets's roll the dice! ")
+total = 0
+num_rolls = int(input("How many times do we want to roll the the dice? "))
+
+def guess(total):
+    
+    guess = int(input("Guess the roll: "))
+    if guess == num_random:
+        total = total +6
+        
+    else:
+        total = total -1
+  
+def rolls(num_rolls, total=0):
+    num_random = random.randint(1,6)
+    if num_rolls== 0:
+        return total
+    else:
+        num_rolls = num_rolls -1
+        guess(num_random,total)
+        print(f"you roll a {num_random}!")
+        return rolls(num_rolls,total)
+            
+total = rolls(num_rolls)
+print(f"the total is: {total}")
+             
+           
