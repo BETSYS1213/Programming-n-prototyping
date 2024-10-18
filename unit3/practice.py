8b2a0e16b773575bf14e0228952ba5d33079b06c
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-10-18 15:05:53 -0400

    Update practice.py

--- unit3/practice.py
+++ unit3/practice.py
@@ -1,5 +1,40 @@
 
+#10/10/2024
+#Betsy Sumba
+#CF7 
 
+
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+num3 = int(input("enter a number: "))
+
+def calc_sum(num1, num2, num3):
+    sum = num1 + num2 + num3
+    print("The sum of you 3 numbers is: "+ str(sum))
+    
+def average3(num1, num2, num):
+    avg = (num1 + num2 +num3)/3
+    print("The average of your 2 number is:"+str(avg))
+    
+calc_sum(num1, num2 ,num3)
+average3(num1, num2, num3)
+calc_sum(2,4,5)
+
+
+
+
+
+#Betsy Sumba
+#10/15/2024
+#period 1,2
+
+delivery = input ("Did you order food? yes or no")
+
+if delivery == "yes" or delivery == "yes" :
+    print("Great")
+    
+else:
+    print("NO?! so who is cooking?")
 
 
 #------ Today---------#
