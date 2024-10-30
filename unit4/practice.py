23219ea61dee65b33a365da33ee9648ee938e112
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-10-30 09:24:37 -0400

    Update practice.py

--- unit4/practice.py
+++ unit4/practice.py
@@ -8,3 +8,48 @@ for i in range(21):
     
 for x in range (10,71,10):
     print(x)
+
+
+#Betsy Sumba
+#10/30/2024
+#1 and 2 
+#CFU12
+def version1():
+    password = "simonsnyc"
+userInput = input("Enter Passcode: ")
+
+while password != userInput :
+    print("Wrong Password!")
+    userInput = input("Enter Passcode: ")
+
+print("Correct! You may enter..") 
+    
+#version1()
+
+def version2():
+    password = "simonsnyc"
+    userInput = input("Enter Passcode: ")
+    num_guesses = 0
+    
+
+while password != userInput and num_guesses < 3 :
+    
+    print("Wrong Password!")
+    userInput = input("Enter Passcode: ")
+    num_guesses += 1
+ if password == "simonsnyc":
+    print("Correct! You may enter..")
+    
+else:
+    print("Wrong Password!")
+    
+version2()    
+    
+    
+
+
+
+
+
+
+
