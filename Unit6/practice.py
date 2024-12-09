a77b92b272614bf533fd25896a90dd9393aeffd8
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-12-09 13:26:26 -0500

    Update practice.py

--- Unit6/practice.py
+++ Unit6/practice.py
@@ -1,30 +1,19 @@
 
+#Betsy Sumba
+#CFU 15
+#1-2
+
 import simplegui
-#betsy Sumba
-#CFU project 
-#12/2/2024
+import random
 
-frame = simplegui.create_frame("Thanksgiving Drawing", 600, 400)
-# Draw handler function
-def draw(canvas):
-    # Example: Drawing a circle
-    canvas.draw_circle((400, 250), 100, 10, "#b17800", "#b15000")
-    canvas.draw_circle((299, 170), 99, 10, "Taupe", "#b15000")
-    canvas.draw_circle((170, 260), 99, 10, "Taupe", "#b15000")
-   
-    #back of the drawing(tail)
-    canvas.draw_circle((300, 200), 100, 10, "#da9349", "Orange")
-    canvas.draw_circle((250, 200), 35, 10, "black", "grey")
-    canvas.draw_circle((350, 200), 35, 10, "black", "grey")
-    canvas.draw_circle((300, 400), 100, 10, "#da9349", "Orange")
-    # Example: Drawing a polygon
-    canvas.draw_polygon([(250, 250), (350, 250), (300, 300)], 5, "#af9549", "Yellow")
-    # Example: Drawing a point
-    canvas.draw_point((200, 500), "Blue")
-   
+def draw_handler(canvas):
+    canvas.draw_polygon([(40, 30), (54, 100),(30, 103)], 5, "black", "black")
+    canvas.draw_polygon([(20,112),(120,150),(90,190)],5 , "black","black" )
+    canvas.draw_polygon([(105, 40), (221,90), (110, 120)], 1, "#black","black")
+       
+    
 
-# Assign draw handler to the frame
-frame.set_draw_handler(draw)
-
-# Start the frame
+frame = simplegui.create_frame("CFU15 Happy Shapes", 200, 200)
+frame.set_canvas_background("White")
+frame.set_draw_handler(draw_handler)
 frame.start()
