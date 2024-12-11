1b4cfb4515c6889d81e763da463e2429a2058c0b
betsy <144722628+BETSYS1213@users.noreply.github.com>
2024-12-11 16:03:15 -0500

    Update practice.py

--- Unit6/practice.py
+++ Unit6/practice.py
@@ -1,19 +1,25 @@
 
 #Betsy Sumba
-#CFU 15
+#12/11/2024
+#CFU 16
 #1-2
 
 import simplegui
-import random
-
 def draw_handler(canvas):
-    canvas.draw_polygon([(40, 30), (54, 100),(30, 103)], 5, "black", "black")
-    canvas.draw_polygon([(20,112),(120,150),(90,190)],5 , "black","black" )
-    canvas.draw_polygon([(105, 40), (221,90), (110, 120)], 1, "#black","black")
-       
+    canvas.draw_circle((100, 95), 80, 3, "Black", "pink")
+    #eyes
+    canvas.draw_circle((65, 90), 25, 3, "black", "white")
+    canvas.draw_circle((140,90), 25, 3, "black" , "white")
+    canvas.draw_circle((70,90),10,3, "black", "black")
+    canvas.draw_circle((131,90),10,3,"black" , "black")
+    #mouth
+    canvas.draw_circle((100,150), 5, 3, "black", "black")
+    canvas.draw_circle((120,142), 5, 3, "black", "black")
+    canvas.draw_circle((85,142), 4, 3, "black" , "black")
     
-
-frame = simplegui.create_frame("CFU15 Happy Shapes", 200, 200)
-frame.set_canvas_background("White")
+    
+   
+frame = simplegui.create_frame("CFU16 Happy circles", 200, 200)
+frame.set_canvas_background("white")
 frame.set_draw_handler(draw_handler)
 frame.start()
