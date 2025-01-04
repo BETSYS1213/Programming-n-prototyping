7c58776df172f873d6a04f5e30177a7748c38160
betsy <144722628+BETSYS1213@users.noreply.github.com>
2025-01-03 20:44:46 -0500

    Update practice.py

--- unit7/practice.py
+++ unit7/practice.py
@@ -1,1 +1,37 @@
+#Betsy Sumba
+#1-2
+#1/2/2025
+#CFU Review + build in formating funtions
+
+box ="cat"
+
+def formating(box):
+    userInput = int(input("choice?(1,2,34,5,6,7,8,9)"))
+    if userInput == 1:
+        #capitaliza 
+        box = box.capitalizel()
+        print(box)
+    elif userInput == 2:
+        #find and returrn the position of a value in thr string
+        box = box.find("i")
+        print(box)
+    elif userInput == 3:
+        # return try if the varr is a number
+        box = box.isdigit()
+        print(box)
+    elif userInput == 4:
+        #output the var all lowercrase
+        box = box.lower
+        print(box)
+    elif userInput == 5:
+        #rep
+        print(box.replace("o","a"))
+    elif userInput == 6:
+        # output all upper case
+        box = box.upper()
+        print(box)
+   
+    else:
+        print("not a valid option")
+formating(box)
 
